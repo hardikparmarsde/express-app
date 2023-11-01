341aa7ede949251a4a4df56cd9738d104a37b547
hardik231 <hrkumar231@gmail.com>
2023-11-01 17:46:07 +0530

    added crud api

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,68 @@
-const { User } = require('../models')
-const createUser = (req, res) => {
-    try {
-        const user = User.create(req.body);
-        res.status(201).json(user)
-    } catch(e) {
-        console.log(e)
-        res.status(500).json({messgae:e})
-    }
-}
+const { User } = require('../models'); 
 
-module.exports = { createUser }
+exports.createUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const user = await User.create({ username, email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to create user' });
+  }
+};
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch users' });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch user' });
+  }
+};
+
+exports.updateUser = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      const { username, email, password } = req.body;
+      await user.update({ username, email, password });
+      res.json(user);
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to update user' });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  const { userId } = req.params;
+
+  try {
+    const user = await User.findByPk(userId);
+    if (!user) {
+      res.status(404).json({ error: 'User not found' });
+    } else {
+      await user.destroy();
+      res.json({ message: 'User deleted successfully' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete user' });
+  }
+};
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,13 @@
 const express = require('express');
-const { createUser } = require('../controllers/userController');
+const { createUser, getAllUsers, updateUser, deleteUser, getUserById } = require('../controllers/userController');
 const userRouter = express.Router();
 
 // Create a new user
-userRouter.post('/users', createUser);
+userRouter.post('/create', createUser);
+userRouter.get('/getAll', getAllUsers);
+userRouter.put('/update/:userId', updateUser);
+userRouter.delete('/delete/:userId', deleteUser);
+userRouter.get('/getSingle/:userId', getUserById);
+
 
 module.exports = userRouter;
